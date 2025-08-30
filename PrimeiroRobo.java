7e4c364d15a65a215444b8bbd0e4fd4187e5412f
vitoriasilvag2005-dev <vitoriasilvag2005@gmail.com>
2025-08-30 18:21:28 -0300

    Update PrimeiroRobo.java

--- PrimeiroRobo.java
+++ PrimeiroRobo.java
@@ -20,31 +20,28 @@ jobs:
           distribution: temurin
           java-version: 11
 
-      - name: Criar diretórios e baixar dependências
+      - name: Criar diretórios e baixar configuração
         run: |
           mkdir -p build libs
-          # Baixar robocode.jar (substitua pela URL correta)
-          wget https://example.com/robocode.jar -O libs/robocode.jar || echo "Robocode já existe ou URL precisa ser ajustada"
           # Baixar configuração do Checkstyle
           wget https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml -O libs/google_checks.xml
 
       - name: Compilar o Robô
         run: |
-          # Compila arquivos específicos - AJUSTE OS NOMES CONFORME SEU PROJETO
-          javac -cp "libs/robocode.jar" -d build PrimeiroRobo.java MeuRobo.java
+          javac -cp "libs/robocode.jar" -d build aprendizado/PrimeiroRobo.java
 
       - name: Rodar Checkstyle
         run: |
+          # URL CORRIGIDA do Checkstyle
           wget https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.4/checkstyle-10.12.4-all.jar -O checkstyle.jar
-          # Analisa arquivos específicos - AJUSTE OS NOMES CONFORME SEU PROJETO
-          java -jar checkstyle.jar -c libs/google_checks.xml PrimeiroRobo.java  || echo "Checkstyle completado (avisos não quebram o build)"
+          java -jar checkstyle.jar -c libs/google_checks.xml aprendizado/PrimeiroRobo.java || echo "Checkstyle completado"
 
       - name: Rodar SpotBugs
         run: |
           wget https://github.com/spotbugs/spotbugs/releases/download/4.8.3/spotbugs-4.8.3.tgz -O spotbugs.tgz
           tar -xvzf spotbugs.tgz
           chmod +x spotbugs-4.8.3/bin/spotbugs
-          ./spotbugs-4.8.3/bin/spotbugs -textui -effort:max -high -auxclasspath libs/robocode.jar build || echo "SpotBugs completado (avisos não quebram o build)"
+          ./spotbugs-4.8.3/bin/spotbugs -textui -effort:max -high -auxclasspath libs/robocode.jar build || echo "SpotBugs completado"
 
       - name: Mensagem final
         run: echo "Pipeline finalizado com sucesso! Código analisado e aprovado."
